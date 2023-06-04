601aa616da83577443505dd538c4875f6c88789e
gaddafi <gaddafiadamu400@gmail.com>
2023-06-04 21:10:31 +0100

    static files conf

--- pycon2017/settings.py
+++ pycon2017/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
